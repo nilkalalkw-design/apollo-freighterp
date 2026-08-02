aca6e6564d19576a554f6262248b074a7bd1dbc3
Apollo Freight <nilkalalkw@gmail.com>
2026-08-02 07:32:22 +0300

    Update APOLLO_API_URL to use meta tag override

--- web/config.js
+++ web/config.js
@@ -1,1 +1,4 @@
-window.APOLLO_API_URL = "https://apollo-freighterp-f9kt.onrender.com";
+(function () {
+  var override = document.querySelector('meta[name="apollo-api-url"]');
+  window.APOLLO_API_URL = (override && override.content && override.content.trim()) || "https://apollo-freighterp-f9kt.onrender.com";
+})();
